c66466301eee2e17e315c5006405600bad10e03b
leandro-wallbit <leandro.rial@wallbit.io>
2022-06-14 19:47:55 -0300

    E4

--- DOM.js
+++ DOM.js
@@ -1,29 +1,70 @@
-const button = document.querySelector("#submit")
+let title = document.getElementById("title");
+let subtitle = document.getElementById("subtitle");
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let card = document.getElementById("card");
 
-const submit = () => {
-    const valueInput = document.querySelector("input").value
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    if (valueInput === '') {
-        alert("Please, complete the field")
+    formValidation();
+});
+
+let formValidation = () => {
+    if (input.value === "") {
+        msg.innerHTML = "Post cannot be blank";
     } else {
-        const pizzaArr = []
-    
-        for (const pizza of Pizzas) {
-            if (Number(valueInput) === pizza.id) {
-                pizzaArr.push(pizza)
-            }
-        }
-
-        if (!pizzaArr.length) {
-            alert("Sorry, but we couldn't find your pizza")
-            document.querySelector('h2').textContent = "";
-            document.querySelector('h4').textContent = "";
-        } else {
-            document.querySelector('h2').textContent = `${pizzaArr[0].name}`;
-            document.querySelector('h4').textContent = `${pizzaArr[0].price}`;
-        }
-    
-        document.querySelector("input").value = "";
+        msg.innerHTML = "";
+        acceptData();
     }
-    
-}
+};
+
+let pizzaObj
+
+let acceptData = () => {
+    valueInput = input.value;
+
+    const Pizzas = JSON.parse(localStorage.getItem("pizzas"));
+
+    for (const pizza of Pizzas) {
+        if (Number(valueInput) === pizza.id) {
+            pizzaObj = pizza
+        }
+    }
+
+    if (!pizzaObj) {
+        msg.innerHTML = "Sorry, but we couldn't find your pizza";
+        title.textContent = "";
+        subtitle.textContent = "";
+        input.value = "";
+    } else {
+        msg.innerHTML = ""
+        title.textContent = `${pizzaObj.name}`;
+        subtitle.textContent = `$${pizzaObj.price}`;
+
+        const { img, ingredients, name, price } = pizzaObj
+
+        const ingredient = ingredients.map(ing => ing)
+
+        let createCard = () => {
+            card.innerHTML += `
+                <div class="pizza-card--content">
+                    <div class="card__picture">
+                        <img src=${img} alt=${name}>
+                    </div>
+                    <main class="card__main">
+                        <p class="main--p">${name}</p>
+                        <div class="main--texts">
+                            <span class="main--span">${ingredient.join(" <br> ")}</span>
+                            <strong class="main--strong">$${price}</strong>
+                        </div>
+                    </main>
+                </div>
+            `;
+            input.value = "";
+        };
+
+        createCard();
+    }
+};
--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 const Pizzas = [
     {
         id: 1,
+        img: "https://mla-s1-p.mlstatic.com/890010-MLA43137481403_082020-F.jpg",
         name: "Muzzarella",
         ingredients: ["tomate", "queso muzzarella"],
         price: 550
     },
     {
         id: 2,
+        img: "https://blog.facilitas.com.ar/wp-content/uploads/2018/11/Blogpost-7-1160x628.jpeg",
         name: "Jamon y morrones",
         ingredients: ["tomate", "queso muzzarella", "jamon", "morron"],
         price: 650
     },
     {
         id: 3,
+        img: "https://t1.rg.ltmcdn.com/es/posts/7/0/2/pizza_fugazza_7207_orig.jpg",
         name: "Fugazzeta",
         ingredients: ["tomate", "queso muzzarella", "cebolla", "queso parmesano", "queso port salut"],
         price: 700
     },
     {
         id: 4,
+        img: "https://t2.rg.ltmcdn.com/es/posts/5/2/6/pizza_napolitana_32625_orig.jpg",
         name: "Napolitana",
         ingredients: ["tomate", "queso muzzarella", "aceitunas", "oregano"],
         price: 850
     },
     {
         id: 5,
+        img: "https://www.recetasderechupete.com/wp-content/uploads/2016/11/pizza_cuatro_quesos.jpg?width=1200&enable=upscale",
         name: "Cuatro quesos",
         ingredients: ["tomate", "queso Muzzarella", "queso Fontina", "queso Parmigiano Reggiano", "queso Gorgonzola"],
         price: 850
     },
     {
         id: 6,
+        img: "https://www.hola.com/imagenes/cocina/recetas/20190911149183/pizza-margarita/0-717-935/pizza-m.jpg",
         name: "Margarita",
         ingredients: ["tomate", "queso Muzzarella", "albahaca"],
         price: 600
     }
 ];
+
+localStorage.setItem('pizzas', JSON.stringify(Pizzas))
 
 let odd = []
 let cheap = []
